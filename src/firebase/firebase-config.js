9f2b94f61aa80c8611320c8f7eef5fc659c66dc0
Leonel Martinez <leonelangelm@gmail.com>
2021-04-12 17:07:29 -0300

    env

--- src/firebase/firebase-config.js
+++ src/firebase/firebase-config.js
@@ -1,19 +1,9 @@
 import firebase from 'firebase'
 import 'firebase/firestore'
 
-//const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CONFIG)
-const firebaseConfig = {
-    apiKey: "AIzaSyDJ8f6S1AAEj51JeDKSMsiGPHI_IOkrmMo",
-    authDomain: "react-inmo.firebaseapp.com",
-    projectId: "react-inmo",
-    storageBucket: "react-inmo.appspot.com",
-    messagingSenderId: "140028720241",
-    appId: "1:140028720241:web:d4a560802f4e90c8bc17d0",
-    measurementId: "G-BPD1J5GPZX"
-};
 
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(process.env.REACT_APP_FIREBASE_CONFIG);
+//firebase.initializeApp(firebaseConfig);
 
 
 
